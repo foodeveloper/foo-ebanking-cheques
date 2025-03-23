1afd03b3a44c0bfde140a4543165a224d39de518
Mac Mini Server <testios@foo-solutions.com>
2025-03-23 13:12:56 +0200

    Push Framework Version: 1.0.5

---
+++ xcframework/FooEBankingCheques.xcframework/ios-arm64/FooEBankingCheques.framework/Headers/FooEBankingCheques.h
@@ -0,0 +1,18 @@
+//
+//  FooEBankingCheques.h
+//  FooEBankingCheques
+//
+//  Created by Elie El Koreh on 1/15/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FooEBankingCheques.
+FOUNDATION_EXPORT double FooEBankingChequesVersionNumber;
+
+//! Project version string for FooEBankingCheques.
+FOUNDATION_EXPORT const unsigned char FooEBankingChequesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FooEBankingCheques/PublicHeader.h>
+
+
